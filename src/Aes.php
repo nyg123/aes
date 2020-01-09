9239af2bc714eaa23d3b453b38db864cbf0c902a
牛永光 <nyg1991@aliyun.com>
2020-01-09 12:03:38 +0800

    增加加密解密函数

--- src/Aes.php
+++ src/Aes.php
@@ -5,6 +5,33 @@ namespace Nyg;
 class Aes
 {
     /**
+     * 可逆解密
+     * @param $data
+     * @param $key
+     * @return mixed
+     * @author 牛永光 nyg1991@aliyun.com
+     * @date   2020/1/9 11:59
+     */
+    static public function decrypt($data, $key)
+    {
+        return json_decode(openssl_decrypt(base64_decode($data), 'aes-128-ecb', $key), true);
+    }
+
+    /**
+     * 可逆加密
+     * @param $data
+     * @param $key
+     * @return string
+     * @author 牛永光 nyg1991@aliyun.com
+     * @date   2020/1/9 12:00
+     */
+    static public function encrypt($data, $key)
+    {
+        $data = json_encode($data);
+        return base64_encode(openssl_encrypt($data, 'aes-128-ecb', $key));
+    }
+
+    /**
      * 生成sign签名
      * @param array  $data 要签名的数据
      * @param string $key
